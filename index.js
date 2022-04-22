63d037385e3a0e209d5b6bb81f72553783b576fc
mohamedbenattia99 <mohamedbenattia99@gmail.com>
2022-04-22 15:35:22 +0100

    testing with jest

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+//app.js
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
